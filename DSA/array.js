5d342a31b2b1e7756325d5462cd0a4e17603b17f
ammara-Ilyas <ismarailyas545@gmail.com>
2024-08-15 21:42:33 +0500

    Array in DSA in javascript

--- DSA/array.js
+++ DSA/array.js
@@ -37,15 +37,34 @@
 // };
 
 // lowestNumber([1, 2, 3, 4, 2, 5, 7, 1, 3, 0]);
-const stringLargestNumber = (sent) => {
-  sent = sent.split(" ");
-  let longestStr = sent[0];
-  sent.forEach((element, i) => {
-    // console.log(element, i);
-    if (element.length > longestStr.length) {
-      longestStr = element;
-    }
-  });
-  console.log("longest word", longestStr);
+// const stringLargestNumber = (sent) => {
+//   sent = sent.split(" ");
+//   let longestStr = sent[0];
+//   sent.forEach((element, i) => {
+//     // console.log(element, i);
+//     if (element.length > longestStr.length) {
+//       longestStr = element;
+//     }
+//   });
+//   console.log("longest word", longestStr);
+// };
+// stringLargestNumber("This is paragraph");
+
+let arr = [0, 1, 2, 4, 5, 6];
+console.log(arr.includes(5));
+console.log("min in array", Math.min(...arr));
+console.log("max in array", Math.max(...arr));
+
+// const intersection = (arr1, arr2) => {
+//   const filtered = arr1.filter((ele, i) => {
+//     return arr2.find((elem) => elem === ele);
+//   });
+//   return filtered;
+// };
+// console.log(intersection([1, 2, 3, 4, 5], [4, 5, 6, 7]));
+
+const unionArr = (arr1, arr2) => {
+  let arr3 = new Set([...arr1, ...arr2]);
+  return Array.from(arr3);
 };
-stringLargestNumber("This is paragraph");
+console.log(unionArr([1, 2, 3, 4], [3, 4, 5, 6]));
